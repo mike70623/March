88e9a64f995b3d8003bc7dfe4c480fb6d0a9ff36
game110pm <game110pm@605-110-STU07>
2017-03-13 12:45:04 -0600

    mario!

---
+++ 3-13-17/3-13-17/koopaling.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctime>
+using namespace std;
+void badguy();
+int main() {
+	srand(time(NULL));
+	while (1) {
+		badguy();
+		system("pause");
+	}
+}
+void badguy() {
+	int x = rand() % 100 + 1;
+	if (x <= 50)
+		cout << "goomba!" << endl;
+	if (x > 50 && x < 80)
+		cout << "koopa!" << endl;
+	if (x > 80 && x <= 100)
+		cout << "shy guy!" << endl;
+}
